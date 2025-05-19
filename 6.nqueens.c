6a3663683efe9f17bf14f7de92c4d6e845109771
Monil Mehta <156000067+Monil110@users.noreply.github.com>
2025-05-19 11:35:13 +0530

    Update 6.nqueens.c

--- 6.nqueens.c
+++ 6.nqueens.c
@@ -4,43 +4,44 @@
 
 #define N 10
 int board[N][N];
+int solutionCount = 0;
 
 bool issafe(int row, int col, int n) {
     for (int i = 0; i < col; i++) {
         if (board[row][i] == 1) return false;
     }
-
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
         if (board[i][j] == 1) return false;
     }
-
-    for (int i = row, j = col; j >= 0 && i < n; i++, j--) {
+    for (int i = row, j = col; i < n && j >= 0; i++, j--) {
         if (board[i][j] == 1) return false;
     }
-
     return true;
 }
 
-bool solve(int n, int col) {
-    if (col >= n) return true;
-
-    for (int i = 0; i < n; i++) {
-        if (issafe(i, col, n)) {
-            board[i][col] = 1;
-            if (solve(n, col + 1))
-                return true;
-            board[i][col] = 0;
-        }
-    }
-    return false;
-}
-
 void print(int n) {
+    printf("Solution %d:\n", ++solutionCount);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%d ", board[i][j]);
         }
         printf("\n");
+    }
+    printf("\n");
+}
+
+void solve(int n, int col) {
+    if (col >= n) {
+        print(n);
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (issafe(i, col, n)) {
+            board[i][col] = 1;
+            solve(n, col + 1); 
+            board[i][col] = 0; 
+        }
     }
 }
 
@@ -55,18 +56,15 @@ int main() {
 
     struct timeval start, end;
     gettimeofday(&start, NULL);
-    if (solve(n, 0)) {
-        gettimeofday(&end, NULL);
-        print(n);
-    } else {
-        gettimeofday(&end, NULL);
-        printf("Solution does not exist\n");
-    }
 
-    long seconds = end.tv_sec - start.tv_sec;
-    long microseconds = end.tv_usec - start.tv_usec;
-    long total_microseconds = seconds * 1000000 + microseconds;
-    printf("Time taken: %ld microseconds\n", total_microseconds);
+    solve(n, 0);
+
+    gettimeofday(&end, NULL);
+
+    double time = (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
+
+    printf("Total solutions: %d\n", solutionCount);
+    printf("Time taken: %.0lf microseconds\n", time);
 
     return 0;
 }
